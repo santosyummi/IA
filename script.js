a039fbd974b053d4f62d864145eaf101c48c2eac
santosyummi <141366425+santosyummi@users.noreply.github.com>
2025-05-23 11:11:06 -0300

    ok

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-rsultado");
+
+const perguntas[
+    {
+        enunciado: "Pergunta1",
+        alternativas: ["alternativa1", "alternativa2"]
+    },
+
+    {
+        enunciado: "Pergunta2",
+        alternativas: ["alternativa1", "alternativa2"]
+    },
+]
