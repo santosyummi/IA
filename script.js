e48b06330d767e7d4cd659b34ea05444b35d6eca
santosyummi <141366425+santosyummi@users.noreply.github.com>
2025-06-06 10:50:23 -0300

    ok

--- script.js
+++ script.js
@@ -3,67 +3,62 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
 const perguntas = [
     {
-        enunciado: "Pergunta1",
+        enunciado: "Como resolver equação do segundo grau?",
         alternativas: [
             {
-                texto: "alternativa1", 
-                afirmacao: "afirmacao1",
+                texto: "aplicar a formula de Bhaskara",
+                afirmacao: "Aplicando a formula de Bhaskara voce pode resolver de forma mais demorada"
+               
             },
             {
-                texto: "alternativa2", 
-                afirmacao: "afirmacao2",
+                texto: "Soma e produto",
+                afirmacao: "usando soma e produto voce pode economizar tempo"
             },
         ]
     },
-
     {
-         
         enunciado: "Pergunta2",
         alternativas: [
             {
-                texto: "alternativa1", 
-                afirmacao: "afirmacao1",
+                texto: "alternativa1",
+                afirmacao: "afirmacao1"
             },
             {
-                texto: "alternativa2", 
-                afirmacao: "afirmacao2",
+                texto: "alternativa2",
+                afirmacao: "afirmacao2"
             },
         ]
     },
-    
-     {
+    {
         enunciado: "Pergunta3",
         alternativas: [
             {
-                texto: "alternativa1", 
-                afirmacao: "afirmacao1",
+                texto: "alternativa1",
+                afirmacao: "afirmacao1"
             },
             {
-                texto: "alternativa2", 
-                afirmacao: "afirmacao2",
+                texto: "alternativa2",
+                afirmacao: "afirmacao2"
             },
         ]
     },
-]
+];
 
 let atual = 0;
 let perguntaAtual;
 
-function mostraPergunta (){
-    perguntaAtual = perguntas [atual];
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
-
 function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas){
+    for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativa = document.createElement("button");
         botaoAlternativa.textContent = alternativa.texto;
         caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
-
 mostraPergunta();
