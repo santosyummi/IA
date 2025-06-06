dbd9b7a8046d2561dbad51a731a34c43434f524c
santosyummi <141366425+santosyummi@users.noreply.github.com>
2025-06-06 11:17:48 -0300

    ok

--- script.js
+++ script.js
@@ -51,6 +51,10 @@ let perguntaAtual;
 let historiaFinal =" "
 
 function mostraPergunta(){
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
