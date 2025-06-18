66e7242eb386e5b8b43a743302885aa2c9f7c92e
santosyummi <141366425+santosyummi@users.noreply.github.com>
2025-06-18 10:54:07 -0300

    ok

--- script.js
+++ script.js
@@ -5,42 +5,43 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
-        enunciado: "Como resolver equação do segundo grau?",
+        enunciado: "Qual a relação entre o desenvolvimento da Fórmula 1 e os avanços tecnológicos que impactam ",
         alternativas: [
             {
-                texto: "aplicar a formula de Bhaskara",
-                afirmacao: "Aplicando a formula de Bhaskara voce pode resolver de forma mais demorada"
+                texto: "A Fórmula 1 impulsiona a pesquisa em aerodinâmica e materiais compostos, como a fibra de carbono.",
+                afirmacao: "As aplicações desses avanços se estendem à aviação e à indústria automotiva de veículos de passeio."
                
             },
             {
-                texto: "Soma e produto",
-                afirmacao: "usando soma e produto voce pode economizar tempo"
+                texto: "O alto investimento em sistemas de telemetria e análise de dados na Fórmula 1 é significativo.",
+                afirmacao: "Esse investimento reflete-se na otimização de processos industriais e no desenvolvimento de softwares de gestão em diversas áreas."
             },
         ]
     },
     {
-        enunciado: "Pergunta2",
+        enunciado: "Como o vôlei exemplifica a importância do trabalho em equipe e da comunicação para o sucesso coletivo?",
         alternativas: [
             {
-                texto: "alternativa1",
-                afirmacao: "afirmacao1"
+                texto: "No vôlei, a coordenação entre levantador, atacantes e defensores é crucial para a construção de uma jogada eficiente.",
+                afirmacao: "Isso demonstra como a colaboração individual contribui para o objetivo comum.
+"
             },
             {
-                texto: "alternativa2",
-                afirmacao: "afirmacao2"
+                texto: "A constante necessidade de comunicação verbal e não verbal entre os jogadores de vôlei é vital para antecipar movimentos e ajustar estratégias.",
+                afirmacao: "Essa comunicação destaca a relevância da interação para a performance do time."
             },
         ]
     },
     {
-        enunciado: "Pergunta3",
+        enunciado: "De que forma o basquete ilustra a necessidade de adaptabilidade tática e estratégica durante uma partida?",
         alternativas: [
             {
-                texto: "alternativa1",
-                afirmacao: "afirmacao1"
+                texto: "A rapidez com que as equipes de basquete alternam entre ataque e defesa é notável.",
+                afirmacao: "A rapidez com que as equipes de basquete alternam entre ataque e defesa é notável."
             },
             {
-                texto: "alternativa2",
-                afirmacao: "afirmacao2"
+                texto: "A variação de jogadas ensaiadas e a improvisação de estratégias em resposta às ações do adversário é uma característica do basquete.",
+                afirmacao: "Isso realça a capacidade dos atletas e treinadores em se adaptar a diferentes cenários de jogo."
             },
         ]
     },
